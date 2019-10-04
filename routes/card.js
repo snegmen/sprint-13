9adcb02f41e96b55f7bd2c5f4107478058afa656
snegmen <che1376@mail.ru>
2019-10-04 15:48:31 +0700

    Job done

--- routes/card.js
+++
@@ -1,8 +0,0 @@
-const cardsRouter = require('express').Router();
-const cards = require('../data/card.json');
-
-cardsRouter.get('/', (req, res) => {
-  res.send(cards);
-});
-
-module.exports = cardsRouter;
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const usersRouter = require('./users.js');
-const cardsRouter = require('./card.js');
-
-module.exports = {
-  usersRouter,
-  cardsRouter,
-};
